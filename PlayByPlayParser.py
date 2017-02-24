b856c393de6e15b531aa47b8c9b36b9f5962c8e1
Josh <jhcotogn@uno.edu>
2017-02-24 11:35:15 -0600

    reformatted regular expression variable names to include _regex

--- PlayByPlayParser.py
+++ PlayByPlayParser.py
@@ -3,26 +3,27 @@ from Player import Player
 
 passingPlay = "7-B.Roethlisberger pass short right to 84-A.Brown pushed ob at PIT 47 for 9 yards (54-D.Hightower)."
 runningPlay = "(15:00) 34-De.Williams right tackle to PIT 38 for 18 yards (54-D.Hightower)."
-interceptionPlay = "(7:09) (No Huddle, Shotgun) 7-B.Roethlisberger pass deep left intended for 88-D.Heyward-Bey INTERCEPTED by 30-D.Harmon at NE 7. 30-D.Harmon ran ob at NE 7 for no gain."
+interceptionPlay = "(7:09) (No Huddle, Shotgun) 7-B.Roethlisberger pass deep left intended for 88-D.Heyward-Bey " \
+                   "INTERCEPTED by 30-D.Harmon at NE 7. 30-D.Harmon ran ob at NE 7 for no gain."
 
-names = re.compile("\d+-[A-z]+\.[A-z]+")        # compiled regex for the first name that appears in the string
-passing = re.compile("pass")                    # compiled regex for passing plays NEED TO ADD SOMETHING FOR INCOMPLETE PASSES
-run = re.compile("guard | tackle | middle | scrambles")     # compiled regex for running plays
-yards = re.compile("for\s(\d+)")                # compiled regex for yardage
-interception = re.compile("INTERCEPTED")
-fumble = re.compile("FUMBLE")
-fumbleLost = re.compile("RECOVERED")            # check to see if fumble is necessary not sure recovered would be used elsewhare
-fumbleNotLost = re.compile('recovered')
-penalty = re.compile("PENALTY")                 # enforced penalties
-sack = re.compile("sacked")
-punt = re.compile("punts")
-kickOff = re.compile("kicks")
-fieldGoalMade = re.compile("field goal is GOOD")
-fieldGoalMissed = re.compile("field goal is No Good")
-incomplete = re.compile("incomplete")
-player_number = re.compile("^(\d+)")
-player_name = re.compile("[A-z]+\.[A-z]+")
-team_name = re.compile("[A-Z][a-z]+\s[A-z]+(?=\sat)")
+names_regex = re.compile("\d+-[A-z]+\.[A-z]+")        # compiled regex for the first name that appears in the string
+passing_regex = re.compile("pass")                    # compiled regex for passing plays NEED TO ADD SOMETHING FOR INCOMPLETE PASSES
+run_regex = re.compile("guard | tackle | middle | scrambles")     # compiled regex for running plays
+yards_regex = re.compile("for\s(\d+)")                # compiled regex for yardage
+interception_regex = re.compile("INTERCEPTED")
+fumble_regex = re.compile("FUMBLE")
+fumble_lost_regex = re.compile("RECOVERED")            # check to see if fumble is necessary not sure recovered would be used elsewhare
+fumble_not_lost_regex = re.compile('recovered')
+penalty_regex = re.compile("PENALTY")                 # enforced penalties
+sack_regex = re.compile("sacked")
+punt_regex = re.compile("punts")
+kick_off_regex = re.compile("kicks")
+field_goal_made_regex = re.compile("field goal is GOOD")
+field_goal_missed_regex = re.compile("field goal is No Good")
+incomplete_regex = re.compile("incomplete")
+player_number_regex = re.compile("^(\d+)")
+player_name_regex = re.compile("[A-z]+\.[A-z]+")
+team_name_regex = re.compile("[A-Z][a-z]+\s[A-z]+(?=\sat)")
 
 
 team_abbreviations = {'Arizona Cardinals': 'ARI',
@@ -71,22 +72,22 @@ def play_by_play_parser(string_to_enter):
     :return:
     """
 
-    if names.search(string_to_enter):
+    if names_regex.search(string_to_enter):
         name_search(string_to_enter)
 
-    if passing.search(string_to_enter):
+    if passing_regex.search(string_to_enter):
         passing_play_parse(string_to_enter)
 
-    if run.search(string_to_enter) and not passing.search(string_to_enter):
+    if run_regex.search(string_to_enter) and not passing_regex.search(string_to_enter):
         # need to work on fumbles but need to work out fumbles recovered vs lost
         running_play_parse(string_to_enter)
 
-    if sack.search(string_to_enter):
+    if sack_regex.search(string_to_enter):
         print("sack")
 
 
 def name_search(string_to_enter):
-    matches = names.findall(string_to_enter)
+    matches = names_regex.findall(string_to_enter)
     number_dash_name_parse(matches[0])
     print("name match: %s" % matches)
     return matches
@@ -97,16 +98,16 @@ def passing_play_parse(string_to_enter):
     temp_list = []
     for i in range(len(names_to_parse)):
         temp_list.append(names_to_parse[i])
-    if interception.search(string_to_enter) and not penalty.search(string_to_enter):
+    if interception_regex.search(string_to_enter) and not penalty_regex.search(string_to_enter):
         print("interception")
         for i in range(4):
             number_dash_name_parse(names_to_parse[i])
-    elif incomplete.search(string_to_enter) and not penalty.search(string_to_enter):
+    elif incomplete_regex.search(string_to_enter) and not penalty_regex.search(string_to_enter):
         print("incomplete")
         for i in range(2):
             number_dash_name_parse(names_to_parse[i])
-    elif not penalty.search(string_to_enter):
-        matches = yards.findall(string_to_enter)
+    elif not penalty_regex.search(string_to_enter):
+        matches = yards_regex.findall(string_to_enter)
         matches.append('0')
         print("passing yds %s" % matches)
         temp_list = []
@@ -117,13 +118,13 @@ def passing_play_parse(string_to_enter):
 
 
 def running_play_parse(string_to_enter):
-    if fumble.search(string_to_enter):
-        if fumbleLost.search(string_to_enter):
+    if fumble_regex.search(string_to_enter):
+        if fumble_lost_regex.search(string_to_enter):
             print("fumble lost")
         else:
             print("fumble not lost")
     else:
-        matches = yards.findall(string_to_enter)
+        matches = yards_regex.findall(string_to_enter)
         matches.append('0')
         print("rushing yds %s" % matches)
         names_to_parse = name_search(string_to_enter)
@@ -138,8 +139,8 @@ def running_play_parse(string_to_enter):
 
 # going to have to work on getting the team name(offensive_team_name) for each player correct.
 def number_dash_name_parse(string_to_parse):
-    number = player_number.findall(string_to_parse)
-    name = player_name.findall(string_to_parse)
+    number = player_number_regex.findall(string_to_parse)
+    name = player_name_regex.findall(string_to_parse)
     key_name = name[0].split(".")[1] + number[0]
     if key_name not in player_objects:
         temp = {key_name: Player(name, number, offensive_team_name)}
@@ -154,8 +155,8 @@ def run_game_log():
     line = test_file.readline()
 
     while line:
-        if team_name.search(line):
-            offensive_team_name = team_name.findall(line)[0]
+        if team_name_regex.search(line):
+            offensive_team_name = team_name_regex.findall(line)[0]
         play_by_play_parser(line)
         line = test_file.readline()
 
